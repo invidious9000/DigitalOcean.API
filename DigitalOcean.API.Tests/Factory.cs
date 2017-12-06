a95feb1a06d0ca0127a499981a1f2ac3ae52fc9d
Mathieu Roy <mroy@ethor.com>
2017-12-05 20:57:02 -0700

    Restore correct code for test Factory and revoke docean API key because I'm an idiot

--- DigitalOcean.API.Tests/Factory.cs
+++ DigitalOcean.API.Tests/Factory.cs
@@ -3,8 +3,7 @@
 namespace DigitalOcean.API.Tests {
     public static class Factory {
         public static IDigitalOceanClient GetClient() {
-          //  return new DigitalOceanClient(Environment.GetEnvironmentVariable("DIGITALOCEAN_API_KEY"));
-            return new DigitalOceanClient("90ac955b4ae5bae49bfbd57bd3231f5ac9d72dc1e0d26ce7b805cdc177956bd6");
+            return new DigitalOceanClient(Environment.GetEnvironmentVariable("DIGITALOCEAN_API_KEY"));
         }
     }
 }
